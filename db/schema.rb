21f2af09467d51078f8d6316b1865895c9abb4ad
Monroe Mann <findingsummer@aol.com>
2023-06-01 18:08:36 -0400

    Added categories; updated navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_19_143935) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_213809) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contact_groupings", force: :cascade do |t|
     t.bigint "contact_id"
