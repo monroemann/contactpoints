73725416e39f5492d1504f9cded6bb9d7033c491
Monroe Mann <findingsummer@aol.com>
2024-02-18 19:17:50 -0500

    Added interaction form validations

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -7,6 +7,15 @@ class Interaction < ApplicationRecord
 
 	validates :user, presence: true
   validates :what_happened, presence: true
+  validates :contact_id, presence: true
+  validates :description, presence: true
+  validates :interaction_type_id, presence: true
+  validates :date, presence: true
+  validates :length, presence: true
+  validates :location_id, presence: true
+  validates :you_initiated_contact, presence: true
+  validate :at_least_one_interaction_category
+  validate :at_least_one_emotional_reaction
 
 	has_many :interact_interact_categories, dependent: :destroy
 	has_many :interaction_categories, through: :interact_interact_categories
@@ -114,6 +123,18 @@ class Interaction < ApplicationRecord
 
   def calculate_points_for_who_initiated_contact
     you_initiated_contact == true ? 1 : 3
+  end
+
+  def at_least_one_interaction_category
+    if interaction_category_ids.blank? || interaction_category_ids.all?(&:blank?)
+      errors.add(:interaction_category_ids, "must have at least one interaction category")
+    end
+  end
+
+  def at_least_one_emotional_reaction
+    if emotional_reaction_ids.blank? || emotional_reaction_ids.all?(&:blank?)
+      errors.add(:emotional_reaction_ids, "must have at least one emotional reaction")
+    end
   end
 
 end
